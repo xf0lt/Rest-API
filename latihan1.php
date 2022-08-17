78d7728aaeec41236f22e26a6f164885b3df26cd
Trysked <trysked@gmail.com>
2022-08-17 10:35:50 +0700

    first commit

---
+++ latihan1.php
@@ -0,0 +1,18 @@
+<?php 
+   $mahasiswa = [
+      [
+         "nama" => "mahasiswa 1",
+         "nrp" => "2121212",
+         "email" => "tes@gmail.com",
+      ],
+      [
+         "nama" => "mahasiswa 2",
+         "nrp" => "2124342",
+         "email" => "testing@gmail.com",
+      ],
+   ];
+
+$data = json_encode($mahasiswa);
+echo $data
+
+?>
